71c094750a1a2d72989075089641a60bf81dd126
chaipat supharak <chaipat.sup@cpf.co.th>
2023-11-15 09:19:57 +0700

    first commit

---
+++ app/src/main/java/com/rudy/kotlin_mvvm_clean_coin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rudy.kotlin_mvvm_clean_coin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
